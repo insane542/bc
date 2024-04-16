82b6025bad3ff9e37209772e389e0a757dfcaeae
insane542 <167103962+insane542@users.noreply.github.com>
2024-04-16 12:24:23 +0530

    Add files via upload

---
+++ 4b_Interfaces.sol
@@ -0,0 +1,14 @@
+pragma solidity >0.8.0;
+interface Calculator {
+   function getResult() external view returns(uint);
+}
+contract Test is Calculator {
+   constructor() {}
+
+   function getResult() external view returns(uint){
+      uint a = 1; 
+      uint b = 2;
+      uint result = a + b;
+      return result;
+   }
+}
